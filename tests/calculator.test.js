d10d6e032b485894a3a80ec8b6b3cf924b59357a
Phanupong_Dex <phanupong.ka@ku.th>
2025-12-22 14:46:13 +0700

    gg

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -17,7 +17,7 @@ describe('Calculator', () => {
         expect(divide(10, 2)).toBe(5);
     });
 
-     test('adds 2 + 3 to equal 5', () => {
+     test('adds -2 + -3 to equal 5', () => {
         expect(add(2, 3)).toBe(5);
     });
 });
